63c7206c7c151e1cb14f49aa3660fd0d90a52c77
tomizawakenshin <kenshin.8799@gmail.com>
2024-10-04 14:05:23 +0900

    DBと連携するための処理完了

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"backend/infra"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	infra.Initialize()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong!",
+		})
+	})
+	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
+}
